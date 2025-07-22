0fc83c6e3cf02986885970bc5047663f6c7b1b25
Rezashamsi <shamsireza368@gmail.com>
2025-07-22 19:08:21 +0430

    account aplication
    the login logout and signup has been done !

--- settings.py
+++ settings.py
@@ -31,11 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'manager',
+    'base',
+    'customer',
+    'main_site',
+    'products',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +81,12 @@ WSGI_APPLICATION = 'Vanda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vanda',
+        'USER': 'root',
+        'PASSWORD': 'Rezash1230!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
